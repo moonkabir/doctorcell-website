9957a60c160e061ec4b227e39703381783c446d2
moonkabir <moon@pencilbox.edu.bd>
2021-02-18 11:55:55 +0600

    new all update and bootstrap menu show on hover

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,16 @@
 // jQuery(document).ready(function () {
 //     jQuery('section nav').meanmenu();
 // });
+
+// jQuery(document).ready(function () {
+//     function bigImg(x) {
+
+//     x.style.height = "64px";
+//     x.style.width = "64px";
+//     }
+
+//     function normalImg(x) {
+//     x.style.height = "32px";
+//     x.style.width = "32px";
+//     }
+// });
